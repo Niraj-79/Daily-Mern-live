512730774acc2d3c9d706d8dc0c1e157d7f0f68e
Niraj79 <nirajkumar50078@gmail.com>
2024-09-27 00:33:29 +0530

    Mern Live

--- 2 JavaScript/5 Loops/Class/styles/6-odd loop.js
+++
@@ -1,6 +0,0 @@
-let another, num;
-do {
-  num = parseInt(prompt("Enter a number: "), 10);
-  console.log(`Square of ${num} is ${num * num}`);
-  another = prompt("Want to enter another (y/n? ");
-} while (another === 'y');
--- 2 JavaScript/9 JSON/Class/style/6-object.js
+++
@@ -1,9 +0,0 @@
-const obj1 = { a: 1 };
-const obj2 = { a: 1 };
-const obj3 = obj1;
-
-console.log(obj1 == obj2); // Output: false (different instances)
-console.log(obj1 == obj3); // Output: true (same instance)
-
-console.log(obj1 === obj2); // Output: false (different instance)
-console.log(obj1 === obj3); // Outpu: true (same instance)
