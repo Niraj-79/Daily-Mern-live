dfd241ea7fda4cfc138e2147ce0ec7a8e4b29908
Niraj79 <nirajkumar50078@gmail.com>
2024-10-23 23:08:59 +0530

    Class 68

---
+++ 5 NodeJs and ExpressJs/4 npm and tools or Errors/Errors and Debugging/syntaxErrors.js
@@ -0,0 +1,11 @@
+// Missing prenthesis in fuction call
+//console.log("Hello, world"
+
+// Unclosed string literal
+// let message = "Welcome to Node.js;
+
+// Improper use of reserved keywords
+// let new = 5;
+
+// Incorrect variable declaration (const needs an inital value)
+// const myVar;
